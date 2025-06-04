2d84ba68f508a8b984828ff677a0b023abfd5201
YannickMyxe <shawn.froste.sf@gmail.com>
2025-06-04 23:13:29 +0200

    Added bismuth and raw bismuth items

--- src/main/java/com/yannickmyxe/neochests/NeoChests.java
+++ src/main/java/com/yannickmyxe/neochests/NeoChests.java
@@ -1,5 +1,7 @@
 package com.yannickmyxe.neochests;
 
+import com.yannickmyxe.neochests.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -36,6 +38,9 @@ public class NeoChests {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        // Register the items to the eventBus
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -48,6 +53,10 @@ public class NeoChests {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.BISMUTH);
+            event.accept(ModItems.RAW_BISMUTH);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
