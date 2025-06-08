24c5a691a680c4338225e03c3890785cc5a21b3d
YannickMyxe <shawn.froste.sf@gmail.com>
2025-06-08 15:48:45 +0200

    Added creative mode tab

--- src/main/java/com/yannickmyxe/neochests/NeoChests.java
+++ src/main/java/com/yannickmyxe/neochests/NeoChests.java
@@ -1,6 +1,7 @@
 package com.yannickmyxe.neochests;
 
 import com.yannickmyxe.neochests.block.ModBlocks;
+import com.yannickmyxe.neochests.item.ModCreativeTabs;
 import com.yannickmyxe.neochests.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
@@ -38,6 +39,9 @@ public class NeoChests {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        // Register the creative mode tab
+        ModCreativeTabs.register(modEventBus);
 
         // Register the items/blocks to the eventBus
         ModItems.register(modEventBus);
---
+++ src/main/java/com/yannickmyxe/neochests/item/ModCreativeTabs.java
@@ -0,0 +1,43 @@
+package com.yannickmyxe.neochests.item;
+
+import com.yannickmyxe.neochests.NeoChests;
+import com.yannickmyxe.neochests.block.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
+
+public class ModCreativeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_TAB =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, NeoChests.MOD_ID);
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_TAB.register(eventBus);
+    }
+
+    public static final Supplier<CreativeModeTab> BISMUTH_ITEMS = CREATIVE_TAB.register("bismuth_items_tab", () ->
+                CreativeModeTab.builder()
+                        .icon(() -> new ItemStack(ModItems.BISMUTH.get()))
+                        .title(Component.translatable("creativetab.neochests.bismuth_items"))
+                        .displayItems((params, output) -> {
+                            output.accept(ModItems.BISMUTH.get());
+                            output.accept(ModItems.RAW_BISMUTH.get());
+                        })
+                .build());
+
+    public static final Supplier<CreativeModeTab> BISMUTH_BLOCKS = CREATIVE_TAB.register("bismuth_blocks_tab", () ->
+            CreativeModeTab.builder()
+                    .withTabsAfter(ResourceLocation.fromNamespaceAndPath(NeoChests.MOD_ID, "bismuth_items_tab"))
+                    .icon(() -> new ItemStack(ModBlocks.BISMUTH_BLOCK.get()))
+                    .title(Component.translatable("creativetab.neochests.bismuth_blocks"))
+                    .displayItems((params, output) -> {
+                        output.accept(ModBlocks.BISMUTH_BLOCK.get());
+                        output.accept(ModBlocks.BISMUTH_ORE_BLOCK.get());
+                    })
+                    .build());
+}
